5c2423d000839cfea027e62ead74b2c43886e958
Terry Cain <tcain@bink.com>
2021-01-14 12:01:15 +0000

    Refactored to send events to elasticsearch

--- attributes/default.rb
+++
@@ -1,4 +0,0 @@
-default['azure_eventhub_logging']['auditlogs_connection_string'] = ''
-default['azure_eventhub_logging']['auditlogs_endpoint'] = ''
-default['azure_eventhub_logging']['oslogs_connection_string'] = ''
-default['azure_eventhub_logging']['oslogs_endpoint'] = ''
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,16 @@ apt_repository 'elasticsearch' do
   distribution 'stable'
   key 'https://artifacts.elastic.co/GPG-KEY-elasticsearch'
   deb_src false
+  action :remove
+end
+
+apt_repository 'elasticsearch' do
+  arch 'amd64'
+  uri 'https://artifacts.elastic.co/packages/oss-7.x/apt'
+  components ['main']
+  distribution 'stable'
+  key 'https://artifacts.elastic.co/GPG-KEY-elasticsearch'
+  deb_src false
 end
 
 package 'journalbeat' do
@@ -17,15 +27,11 @@ service 'journalbeat' do
   action :enable
 end
 
-template '/etc/journalbeat/journalbeat.yml' do
-  source 'journalbeat.yml.erb'
+cookbook_file '/etc/journalbeat/journalbeat.yml' do
+  source 'journalbeat.yml'
   owner 'root'
   group 'root'
   mode '0600'
   action :create
   notifies :restart, 'service[journalbeat]'
-  variables(
-    connection_string: node['azure_eventhub_logging']['oslogs_connection_string'],
-    endpoint: node['azure_eventhub_logging']['oslogs_endpoint']
-  )
 end
